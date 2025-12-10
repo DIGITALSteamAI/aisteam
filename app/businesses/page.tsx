639a38d71955f7cab4abee1de4de0ccd588c4680
DIGITALSteamAI <danny@digitalsteam.ca>
2025-12-10 13:36:48 -0500

    Remove old businesses page - replaced by CRM/clients module

--- app/businesses/page.tsx
+++
@@ -1,9 +0,0 @@
-import PageWrapper from "../components/PageWrapper";
-
-export default function Page() {
-  return (
-    <PageWrapper title="Businesses" infoPage="businesses">
-      <p>Your business records will appear here.</p>
-    </PageWrapper>
-  );
-}
